7aa06077595997bf7316c1bf06180f3626debb4b
Vladislav Dragushin <maxhc@inbox.ru>
2022-09-09 19:11:40 +0300

    added shorten_link func

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ from dotenv import load_dotenv
 
 URL_TEMPLATE = 'https://api-ssl.bitly.com'
 
-def main():
-    butly_prefix = '/v4/bitlinks'
-    url = urljoin(URL_TEMPLATE, butly_prefix)
-    print(url)
-    load_dotenv()
-    token = os.getenv('TOKEN')
 
+def shorten_link(token, url):
     headers = {
         'Authorization': token,
     }
-
     payload = {
         'long_url': 'https://python-scripts.com/json',
     }
 
     response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()
-    print(response.json())
+
+    return response.json()
+
+
+def main():
+    butly_prefix = '/v4/bitlinks'
+    url = urljoin(URL_TEMPLATE, butly_prefix)
+
+    load_dotenv()
+    token = os.getenv('TOKEN')
+
+    print('Битлинк', shorten_link(token, url)['link'])
 
 
 if __name__ == '__main__':
