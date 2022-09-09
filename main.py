6ad433597bfe3439f039dd5d8d480723af8bbd00
Vladislav Dragushin <maxhc@inbox.ru>
2022-09-09 21:31:10 +0300

    added user input

--- main.py
+++ main.py
@@ -11,12 +11,15 @@ from dotenv import load_dotenv
 URL_TEMPLATE = 'https://api-ssl.bitly.com'
 
 
-def shorten_link(token, url):
+def shorten_link(token, user_input):
+    butly_prefix = '/v4/bitlinks'
+    url = urljoin(URL_TEMPLATE, butly_prefix)
+
     headers = {
         'Authorization': token,
     }
     payload = {
-        'long_url': 'https://python-scripts.com/json',
+        'long_url': user_input,
     }
 
     response = requests.post(url, headers=headers, json=payload)
@@ -26,13 +29,12 @@ def shorten_link(token, url):
 
 
 def main():
-    butly_prefix = '/v4/bitlinks'
-    url = urljoin(URL_TEMPLATE, butly_prefix)
+    user_input = input()
 
     load_dotenv()
     token = os.getenv('TOKEN')
 
-    print('Битлинк', shorten_link(token, url)['link'])
+    print('Битлинк', shorten_link(token, user_input)['link'])
 
 
 if __name__ == '__main__':
